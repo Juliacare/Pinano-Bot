0b3c822e8ccaa24bc6baba087ff3597e626f973d
Papa Uber <gagepielsticker@gmail.com>
2019-01-17 01:08:12 -0600

    Update time.js

--- commands/general/time.js
+++ commands/general/time.js
@@ -5,8 +5,8 @@ module.exports.load = (client) => {
                 if(res == null){client.errorMessage(message, "Error fetching your data from our servers, please try again."); return}
                 let msg = new client.discord.RichEmbed()
                 msg.setAuthor(`${message.author.username}#${message.author.discriminator}'s practice time stats`, `${message.author.avatarURL}`)
-                msg.addField('Weekly Practice Time', `\`${client.hd(res.current_session_playtime * 1000)}\``, false)
-                msg.addField('Overall Practice Time', `\`${client.hd(res.overall_session_playtime * 1000)}\``, false)
+                msg.addField('Weekly Practice Time', `\`${client.hd(res.current_session_playtime * 1000, { units: ['h', 'm', 's'] })}\``, false)
+                msg.addField('Overall Practice Time', `\`${client.hd(res.overall_session_playtime * 1000, { units: ['h', 'm', 's'] })}\``, false)
                 msg.setColor(client.settings.embed_color)
                 msg.setTimestamp()
                 client.fetchWeeklyLeaderboardPos(message, pos => {
